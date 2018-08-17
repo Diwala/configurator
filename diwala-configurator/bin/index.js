abbe9be9d39ca0e434c90b5bdc60b18f9dcb004f
Malith Wijenayake <malithrw@gmail.com>
2018-08-17 15:24:43 +0530

    Migrate into using promises instead of callbacks

--- diwala-configurator/bin/index.js
+++ diwala-configurator/bin/index.js
@@ -20,41 +20,39 @@ program
     console.log('    $ setup-web -e dev -t d589d6c3dc87d0df365110f12ce22d5b37b5awds');
     console.log();
   })
-  .action((options) => {
+  .action(async (options) => {
     // Validate token
     const tokenValidateSpinner = ora('Validating Github API token.').start();
-    setup.validateToken(options.token, (err, res) => {
-      if (res.status === 200) {
-        tokenValidateSpinner.succeed(res.message);
+    const validatedResponse = await setup.validateToken(options.token);
+    if (validatedResponse.status === 200) {
+      tokenValidateSpinner.succeed(validatedResponse.message);
 
-        ora('Initializing configurator...').info();
-        const selectedEnv = _.findKey(Environment, o => o === options.environment);
-        if (typeof selectedEnv === 'undefined') {
-          ora('Unknown enviroment selected. Please check and try again.').fail();
-          process.exit(1);
-        }
-
-        // Download web configs
-        const config = getConfigSettings(Platform.Web, Environment[selectedEnv]);
-        if (!config) {
-          ora(`Missing configurations for ${options.environment} ${Platform.Web}!`).fail();
-          process.exit(1);
-        }
-        config.configs.forEach((file) => {
-          const streamSpinner = ora(`Downloading ${config.environment} ${file.type} configuration file for ${config.platform}`).start();
-          setup.downloadFile(file, options.token, (e, r) => {
-            if (e) {
-              streamSpinner.fail(r.message).frame();
-            } else {
-              streamSpinner.succeed(r.message);
-            }
-          });
-        });
-      } else {
-        tokenValidateSpinner.fail(res.message);
+      ora('Initializing configurator...').info();
+      const selectedEnv = _.findKey(Environment, o => o === options.environment);
+      if (typeof selectedEnv === 'undefined') {
+        ora('Unknown enviroment selected. Please check and try again.').fail();
         process.exit(1);
       }
-    });
+
+      // Download configs
+      const config = getConfigSettings(Platform.Web, Environment[selectedEnv]);
+      if (!config) {
+        ora(`Missing configurations for ${options.environment} ${Platform.Web}!`).fail();
+        process.exit(1);
+      }
+      config.configs.forEach(async (file) => {
+        const streamSpinner = ora(`Downloading ${config.environment} ${file.type} configuration file for ${config.platform}`).start();
+        const downloadResponse = await setup.downloadFile(file, options.token);
+        if (downloadResponse.status === 200) {
+          streamSpinner.succeed(downloadResponse.message);
+        } else {
+          streamSpinner.fail(downloadResponse.message).frame();
+        }
+      });
+    } else {
+      tokenValidateSpinner.fail(validatedResponse.message);
+      process.exit(1);
+    }
   });
 
 program.parse(process.argv);
--- diwala-configurator/lib/setup.js
+++ diwala-configurator/lib/setup.js
@@ -1,29 +1,31 @@
 const fs = require('fs');
-const request = require('request');
+const axios = require('axios');
 const { REPO } = require('./configs');
 
 /**
  * Validate Github API token
  *
  * @param {String} token Github API token
- * @param {Function} cb Callback function
  */
-const validateToken = (token, cb) => {
-  request.get({
-    url: REPO,
-    headers: {
-      Authorization: `token ${token}`,
-      'User-Agent': 'request',
-    },
-  }, (err, res) => {
-    if (err) {
-      cb(err, { status: 400, message: 'Unable to receive a response. Probably a network issue!' });
-    } else if (res.statusCode === 200) {
-      cb(null, { status: res.statusCode, message: 'Github API token validated!' });
-    } else if (res.statusCode === 401) {
-      cb(err, { status: res.statusCode, message: 'Invalid Github API token.' });
+const validateToken = async (token) => {
+  try {
+    const response = await axios({
+      method: 'GET',
+      url: REPO,
+      headers: {
+        Authorization: `token ${token}`,
+      },
+    });
+    if (response.status === 200) {
+      return { status: response.status, message: 'Github API token validated!' };
     }
-  });
+    return { status: response.status, message: 'Something isn\'n right! :(' };
+  } catch (error) {
+    if (!error.response) {
+      return { status: 500, message: 'Didn not receive a response. Probably a network issue!' };
+    }
+    return { status: error.response.status, message: 'Invalid Github API token.' };
+  }
 };
 
 /**
@@ -32,27 +34,31 @@ const validateToken = (token, cb) => {
  * @param {String} fileObj.source Path to download file
  * @param {String} fileObj.destination Path to store downloaded file
  * @param {String} token Github API token
- * @param {Function} cb Callback function
  */
-const downloadFile = (fileObj, token, cb) => {
+const downloadFile = async (fileObj, token) => {
   const file = fs.createWriteStream(fileObj.destination);
-  request.get({
-    url: fileObj.source,
-    headers: {
-      Authorization: `token ${token}`,
-      'User-Agent': 'request',
-      Accept: 'application/vnd.github.v3.raw',
-    },
-  }, (err) => {
-    if (err) {
-      cb(err, { status: 400, message: `Unable to download ${fileObj.source}` });
+  return new Promise(async (resolve) => {
+    try {
+      const response = await axios({
+        method: 'GET',
+        url: fileObj.source,
+        headers: {
+          Authorization: `token ${token}`,
+          Accept: 'application/vnd.github.v3.raw',
+        },
+        responseType: 'stream',
+      });
+      if (response) {
+        response.data.pipe(file);
+        file.on('finish', () => {
+          resolve({ status: 200, message: `Downloaded and saved the ${fileObj.type} config into ${fileObj.destination}` });
+        }).on('error', () => {
+          resolve({ status: 500, message: `Unable to save the ${fileObj.type} config into ${fileObj.destination}` });
+        });
+      }
+    } catch (error) {
+      resolve({ status: 500, message: `Unable to download ${fileObj.source}` });
     }
-  }).pipe(file);
-
-  file.on('finish', () => {
-    cb(null, { status: 200, message: `Downloaded and saved the ${fileObj.type} config into ${fileObj.destination}` });
-  }).on('error', (err) => {
-    cb(err, { status: 500, message: `Unable to save the ${fileObj.type} config into ${fileObj.destination}` });
   });
 };
 
