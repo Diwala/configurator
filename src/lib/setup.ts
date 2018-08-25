4e336b5a5a3d7c4402e307e83bebc367fcccd805
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2018-08-25 16:00:22 +0200

    Fixed up in the structure, and using only a promise for where needed in that method

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -47,54 +47,65 @@ export const validateToken = async (token:string, repo: string) => {
  * @param {String} fileObj.destination Path to store downloaded file
  * @param {String} token Github API token
  */
+export const getConfigs = async (token: string, env: string, repo: string, service: string, branch: string) => {
   initGithubService(token);
+  try {
+    const responseContent = await getContentFromRepo(repo, service, branch);
+    const folder = responseContent.data.find((folder)=>{
+      return folder.name === env;
+    })
 
+    const responseTree = await getGitTreeFromRepo(repo, folder.sha, branch);
+    const files = responseTree.data.tree.filter((treeObject) => {
+      return treeObject.type === 'blob'
+    })
+
+    const status = ora(`Downloading configuration file[s] for ${service}`).start();
+    const fileStreams = files.map(async (file) => {
+      const stream = await getFileFromRepo(file.url);
+      const path = file.path;
+      return { stream, path }
+    })
+
+    const resolvedFileStreams = await Promise.all(fileStreams);
+    status.succeed()
+
+    const fileResponse = resolvedFileStreams.map(async (stream) => {
+      const newStatus = ora(`Saving config file to ${stream.path}`).start();
+      try {
+        const response = await pipeFile(stream, service);
+        newStatus.succeed();
+        return response
+      } catch(e) {
+        newStatus.fail(e.message)
+        throw e
+      }
+    })
+    const resolvedFileResponse = await Promise.all(fileResponse);
+
+
+  } catch (error) {
+    if(error.type === ErrorTypes.Service) {
+      const stack = error.stack.split('\n').slice(1).join('\n');
+      throw new CLIError(`${error.message} with status ${error.status} with trace ${stack}`);
+    } else {
+      throw error
+    }
+  }
+};
+
+const pipeFile = async (stream, service) => {
   return new Promise(async (resolve, reject) => {
     try {
-      const responseContent = await getContentFromRepo(repo, service, branch);
-      const folder = responseContent.data.find((folder)=>{
-        return folder.name === env;
-      })
-
-      const responseTree = await getGitTreeFromRepo(repo, folder.sha, branch);
-      const files = responseTree.data.tree.filter((treeObject) => {
-        return treeObject.type === 'blob'
-      })
-
-      const fileStreams = files.map(async (file) => {
-          const stream = await getFileFromRepo(file.url);
-          const path = file.path.pathss.path;
-          return { stream, path }
-      })
-
-      const resolvedFileStreams = await Promise.all(fileStreams);
-
-      const fileResponse = resolvedFileStreams.forEach((stream) => {
-        try {
-          const file = fs.createWriteStream(stream.path);
-          stream.stream.data.pipe(file);
-          file.on('finish', () => {
-            resolve({ status: 200, message: `Downloaded and saved the ${service} config into ${stream.path}` });
-          }).on('error', (err) => {
-            reject({ status: 500, message: `Unable to save the ${service} config into ${stream.path}` });
-          });
-        } catch(e) {
-          throw e
-        }
-      })
-    } catch (error) {
-      if(error.type === ErrorTypes.Service) {
-        const stack = error.stack.split('\n').slice(1).join('\n');
-        reject(new CLIError(`${error.message} with status ${error.status} with trace ${stack}`))
-      } else {
-        reject(error)
-      }
+      const file = fs.createWriteStream(stream.path);
+      stream.stream.data.pipe(file);
+      file.on('finish', () => {
+        resolve();
+      }).on('error', (err) => {
+        reject({ status: 500, message: `Unable to save the ${service} config into ${stream.path}` });
+      });
+    } catch(e) {
+      throw e
     }
-  });
-};
-
-module.exports = {
-  validateToken,
-  downloadFile,
-  getConfigs
-};
+  })
+}
